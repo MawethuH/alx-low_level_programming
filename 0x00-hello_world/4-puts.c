6d4907fbab14472f3b8251696934bf73fafacf0f
MawethuH <hlophemawethu@gmail.com>
2023-07-13 11:17:10 -0700

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * Start with entry point
- * The main takes no parameters
- * It returns an integer
- * Puts function outputs the string
+ * main: prints a string using puts function
+ * 
+ * Return: 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return(0);
 }
