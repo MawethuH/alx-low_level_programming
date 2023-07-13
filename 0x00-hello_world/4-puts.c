220e749a8614bc1d070a82ff7abe8fb998d45d8b
MawethuH <hlophemawethu@gmail.com>
2023-07-13 10:56:41 -0700

    Undoing chmod

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ * Start with entry point
+ * The main takes no parameters
+ * It returns an integer
+ * Puts function outputs the string
+ */
 int main(void)
 {
-	puts("\ "Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
 }
