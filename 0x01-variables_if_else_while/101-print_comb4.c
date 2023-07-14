0acd6678c1968b19a7dcf7028448f6c67b317735
MawethuH <hlophemawethu@gmail.com>
2023-07-14 12:13:12 -0700

    Combo of three

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,17 +8,19 @@ int main(void)
 	int a, b, c;
 
 
-	for (a = 0; a < 8; a++)
+	for (a = '0'; a < '9'; a++)
 	{
-		for (b = a + 1; b < 9; b++)
+		for (b = a + 1; b <= '9'; b++)
 		{
-			for (c = a + 1; c < 10; c++)
+			for (c = a + 1; c <= '9'; c++)
 			{
-				if (a != b && b != c && a != c)
+				if ((b != a) != c)
 				{
-					putchar(a + '0');
-					putchar(b + '0');
-					putchar(c + '0');
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a == '7' && b == '8')
+						continue;
 					putchar(',');
 					putchar(' ');
 				}
