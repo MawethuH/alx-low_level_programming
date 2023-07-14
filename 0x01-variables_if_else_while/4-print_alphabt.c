587a7f7fd4402a9083580e698d09a63d4e1c3c14
MawethuH <hlophemawethu@gmail.com>
2023-07-14 06:43:25 -0700

    Missing alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,9 +7,10 @@ int main(void)
 {
 	char i;
 
-	for (i = 'a'; (i != 'e' && i != 'q'); i++)
+	for (i = 'a'; i < 'z'; i++)
 	{
-		putchar(i);
+		if (i != 'e' && i != 'q')
+			putchar(i);
 	}
 	putchar('\n');
 	return (0);
