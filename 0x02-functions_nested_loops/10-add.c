e7d9c5de8c56bb4734e8eab97b7da645af75b7d9
MawethuH <hlophemawethu@gmail.com>
2023-07-18 08:00:20 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * add - adds two intergers.
+ * @a: parameter.
+ * @b: parameter.
+ * Return: sum.
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
