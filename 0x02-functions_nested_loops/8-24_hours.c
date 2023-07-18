755f2a72b1899407c6398048d7f46a041fb714a8
MawethuH <hlophemawethu@gmail.com>
2023-07-18 06:58:20 -0700

    Counting hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,13 +12,17 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2 ; a++)
 	{
+		_putchar(a + '0');
 		for (b = 0; b <= 9; b++)
 		{
+			_putchar(b + '0');
 			_putchar(':');
 			for (c = 0; c <= 5; c++)
 			{
+				_putchar(c + '0');
 				for (d = 0; d <= 9; d++)
 				{
+					_putchar(d + '0');
 					_putchar('\n');
 				}
 			}
