1a943720fbf65d11b8668141f58987153f182d7c
MawethuH <hlophemawethu@gmail.com>
2023-07-18 05:42:06 -0700

    Check sign of int

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of an interger.
+ * @n: the parameter.
+ *Return: 1 for +ve, 0 for zero and -1 for -ve.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
