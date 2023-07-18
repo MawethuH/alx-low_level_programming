52956118698a55dd19863e2023d0047db7183cc1
MawethuH <hlophemawethu@gmail.com>
2023-07-18 13:03:29 -0700

    Times tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,20 +18,32 @@ void print_times_table(int n)
 			{
 				k = i * j;
 
-				if (k <= 99)
-					_putchar(' ');
-				if (k <= 9)
-					_putchar(' ');
-				if (k >= 100)
+				if (j == 0)
+					_putchar(k + '0');
+				if (k < 10 && j != 0)
 				{
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
 				}
-				else if (k <= 99 && k >= 10)
+				if (k >= 10 && k < 100)
 				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
 				}
-				_putchar((k % 10) + '0');
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
 			_putchar('\n');
 		}
