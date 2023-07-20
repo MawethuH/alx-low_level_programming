75bb3663ed10c5ecc1bbfbadbfcab4b4036f4c87
MawethuH <hlophemawethu@gmail.com>
2023-07-20 05:40:51 -0700

    Print a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,6 +10,9 @@ void print_square(int size)
 {
 	int i, j;
 
+	if (size > 0)
+	{
+
 	for (i = 1; i <= size; i++)
 	{
 		for (j = 1; j <= size; j++)
@@ -18,4 +21,7 @@ void print_square(int size)
 		}
 		_putchar('\n');
 	}
+	}
+	else
+		_putchar('\n');
 }
