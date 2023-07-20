755052a65df973479ce38c69a7f00795f59a1c1d
MawethuH <hlophemawethu@gmail.com>
2023-07-20 08:08:53 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,15 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz ");
-		else if (i % 3 == 0)
+		else if ((i % 3 == 0) && (i % 5 != 0))
 			printf("Fizz ");
-		else if ((i % 5 == 0))
+		else if ((i % 5 == 0) && (i % 3 != 0))
 			printf("Buzz ");
-		else if (i == 100)
-			printf("\n");
+		else if (i == 1)
+			printf("%d", i);
 		else
-			printf("%d ", i);
+			printf(" %d", i);
 	}
+	printf("\n");
 	return (0);
 }
