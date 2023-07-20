a6b7c612156958a03afc86cc9bc3be986c0589a6
MawethuH <hlophemawethu@gmail.com>
2023-07-20 03:49:34 -0700

    Multiply two intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * mul - multiplies two integers
+ * @a: parameter
+ * @b: parameter
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
