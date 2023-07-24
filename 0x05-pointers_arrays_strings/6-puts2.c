6c56aaf30839550d299ed72f9e888d16408d4be3
MawethuH <hlophemawethu@gmail.com>
2023-07-24 10:03:35 -0700

    Prints some characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * put2 - prints every other character.
+ * @str: input string.
+ */
+
+void put2(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != 0)
+		len++;
+	for (; i <= len; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
