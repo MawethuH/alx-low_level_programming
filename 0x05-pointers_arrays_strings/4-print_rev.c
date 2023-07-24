bbb2526cbb57b07a80d89556abb3e8f966d4346a
MawethuH <hlophemawethu@gmail.com>
2023-07-24 09:34:11 -0700

    Print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,13 @@
 void print_rev(char *s)
 {
 	int len = 0;
+	int i;
 
 	while (s[len])
 		len++;
-	while (len)
+	for (i = len + 1; i >= 0; i--)
 	{
-		_putchar(s[len--]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
