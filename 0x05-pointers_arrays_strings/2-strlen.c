595c7d425cf6f569ac568374ce662c4e6a99fb24
MawethuH <hlophemawethu@gmail.com>
2023-07-24 07:28:01 -0700

    String lenth

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _strlen - lenth of a string.
+ * @s: Address of the string.
+ * Return: String length.
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = sizeof(*s);
+	return (len);
+}
