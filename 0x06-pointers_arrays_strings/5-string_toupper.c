619c0722a0deab1e0734e76d135f55b9489b5c1d
MawethuH <hlophemawethu@gmail.com>
2023-07-27 07:49:35 -0700

    String to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - changes lower to upper.
+ * @s: string pointer.
+ * Return: character.
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
