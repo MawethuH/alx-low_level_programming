392ebe5a8ed7eb6ba3335456673168cc5798d549
MawethuH <hlophemawethu@gmail.com>
2023-07-31 07:13:11 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character.
+ * @s: string.
+ * @c: character being searched.
+ * Return: character.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
