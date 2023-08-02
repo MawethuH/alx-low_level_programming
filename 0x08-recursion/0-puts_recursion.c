669aafc44be160e5ea7277d461448efc05d30d75
MawethuH <hlophemawethu@gmail.com>
2023-08-02 09:58:49 -0700

    Puts with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,12 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] != '\0')
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
-		_puts_recursion(s[i + 1]);
+		_putchar('\n');
+		return;
 	}
-	_putchar('\0');
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
