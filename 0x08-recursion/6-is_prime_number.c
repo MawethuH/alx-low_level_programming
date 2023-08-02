f7a3bd41251a0a1dd0468f08a928f7395152043f
MawethuH <hlophemawethu@gmail.com>
2023-08-02 11:12:23 -0700

    Prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+int check_prime(int n, int i);
+
+/**
+ * is_prime_number - entry point.
+ * @n: input.
+ *Return: interger.
+ */
+
+int is_prime_number(int n)
+{
+	return (check_prime(n, 1));
+}
+
+/**
+ * check_prime - checks if number is prime.
+ * @n: input.
+ * @i: input.
+ * Return: int.
+ */
+
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+	if (n % i == 0 && i > 1)
+		return (0);
+	if ((n / i) < 1)
+		return (1);
+	return (check_prime(n, i + 1));
+}
