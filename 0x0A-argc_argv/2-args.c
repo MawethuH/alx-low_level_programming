4f523ae18039e2ad72ebe05e63d6e5a3e93dea92
MawethuH <hlophemawethu@gmail.com>
2023-08-04 06:59:49 -0700

    Print arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry.
+ * @argc: count.
+ * @argv: strings.
+ * Return: int.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	(void) argv;
+
+	for (; i < argc; i++)
+		printf("%s\m", argv[i]);
+	return (0);
+}
