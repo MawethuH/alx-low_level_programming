c85fa81d257b2bd5d3589770dbf26cc96df3938c
MawethuH <hlophemawethu@gmail.com>
2023-08-04 06:24:05 -0700

    Number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry.
+ * @argc: count.
+ * @argv: string.
+ * Return: int.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+			sum += sum;
+		printf("%d", sum);
+	}
+	return (0);
+}
