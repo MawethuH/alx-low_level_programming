85c1379f903cd910e44877662c940461fb006b6c
MawethuH <hlophemawethu@gmail.com>
2023-08-04 06:08:24 -0700

    What's my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry.
+ * @argc: counter.
+ * @argv: values.
+ * Return: interger.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
