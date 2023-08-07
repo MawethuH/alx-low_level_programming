3dd8b9cf331e93cd5373ceac97573b907bd45996
MawethuH <hlophemawethu@gmail.com>
2023-08-07 09:44:45 -0700

    Strtow

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,95 @@
+#include "main.h"
+#include <stdlib.h>
+
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
+
+/**
+ * word_len - entry.
+ * @str: input.
+ * Return: int.
+ */
+
+int word_len(char *str)
+{
+	int i = 0, l = 0;
+
+	while (*(str + i) && *(str + i) != ' ')
+	{
+		l++;
+		i++;
+	}
+	return (len);
+}
+
+/**
+ * count_words - entry.
+ * @str: input.
+ * Return: int.
+ */
+
+int count_words(char *str)
+{
+	int i = 0, words = 0, l = 0;
+
+	for (i = 0; *(str + i); i++)
+		l++;
+
+	for (i = 0; i < l; i++)
+	{
+		if (*(str + i) != ' ')
+		{
+			words++;
+			i += word_len(str + i);
+		}
+	}
+	return (words);
+}
+
+/**
+ * strtow - entry.
+ * @str: input.
+ * Return: char.
+ */
+
+char **strtow(char *str)
+{
+	char **strings;
+	int index = 0, words, w, letters, l;
+
+	if (str == NULL || str[0]  == '\0')
+		return (NULL);
+	words = count_words(str);
+	if (words == 0)
+		return (NULL);
+
+	strings = malloc(sizeof(char *) * (words + 1));
+	if (strings == NULL)
+		return (NULL);
+
+	for (w = 0; w < words; w++)
+	{
+		while (str[index] == ' ')
+			index++;
+		letters = word_len(str + index);
+		strings[w] = malloc(sizeof(char) * (letters + 1));
+
+		if (strings[w] == NULL)
+		{
+			for (; w >= 0; w--)
+				free(strings[w]);
+
+			free(strings);
+			return (NULL);
+		}
+
+		for (l = 0; l < letters; l++)
+			strings[w][l] = str[index++];
+
+		strings[w][l] = '\0';
+	}
+
+	strings[w] = NULL;
+	return (strings);
+}
