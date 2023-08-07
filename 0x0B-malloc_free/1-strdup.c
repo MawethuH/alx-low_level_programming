af8efd3cf11c74b524dddb69fb601fa21d155ea3
MawethuH <hlophemawethu@gmail.com>
2023-08-07 08:10:02 -0700

    Strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,8 +9,25 @@
 
 char *_strdup(char *str)
 {
-	char *p = (char)malloc(sizeof(char) * sizeof(str));
+	int a = 0, i = 1;
+	char *s;
 
-	p = str;
-	return (p);
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+
+	s = malloc((sizeof(char) * i) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	while (a < i)
+	{
+		s[a] = str[a];
+		a++;
+	}
+
+	s[a] = '\0';
+	return (s);
 }
