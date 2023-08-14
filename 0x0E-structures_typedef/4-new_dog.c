b7a1b3b04f751e6ca4fbfd9f25d8561e13c8637d
MawethuH <hlophemawethu@gmail.com>
2023-08-14 15:39:28 -0700

    New dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -28,17 +28,22 @@ int _strlen(char *s)
  * Return: pointer to string.
  */
 
-char _strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int len, i;
 
 	len = 0;
 
 	while (src[len] != '\0')
+	{
 		len++;
+	}
 
 	for (i = 0; i < len; i++)
+	{
 		dest[i] = src[i];
+	}
+
 	dest[i] = '\0';
 	return (dest);
 }
