1d061b80f7809246bf75443fa76be3d8cd31af96
MawethuH <hlophemawethu@gmail.com>
2023-08-29 10:19:03 -0700

    Add nodeint

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *add_nodeint - entry.
+ *@head: input.
+ *@n: input.
+ *Return: head.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *added;
+
+	added = malloc(sizeof(listint_t));
+	if (added == NULL)
+		return (NULL);
+	added->n = 0;
+	added->next = *head;
+	*head = added;
+	return (added);
+}
