4805830c6f98d0677073767fcc70edee1d416728
MawethuH <hlophemawethu@gmail.com>
2023-08-29 11:46:44 -0700

    Delete

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - ...
+ * @head: ...
+ * @index: ...
+ * Return: ...
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp, *cpy = *head;
+	unsigned int i;
+
+	if (cpy == NULL)
+	{
+		return (-1);
+	}
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(cpy);
+		return (1);
+	}
+
+	for (i = 0; i <= (idx - 2); i++)
+	{
+		if (cpy->next == NULL)
+			return (-1);
+		cpy = cpy->next;
+	}
+
+	tmp = cpy->next;
+	cpy->next = tmp->next;
+	free(tmp);
+	return (1);
+}
