440479182034b153ca45ca99cf2fbee8f5132db1
MawethuH <hlophemawethu@gmail.com>
2023-09-01 05:54:26 -0700

    Flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,11 +12,13 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int xor = n ^ m, num;
 	int j, i = 0;
 
-	for (j = 63; j >= 0; j--)
+	j = 63;
+	while (j >= 0)
 	{
 		num = xor >> j;
 		if (num & 1)
 			i++;
+		j--;
 	}
 	return (i);
 }
