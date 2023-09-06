bbffeb7bbab927a7f7dcf7e42aa738bf101e0807
MawethuH <hlophemawethu@gmail.com>
2023-09-06 14:38:54 -0700

    Read text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * read_textfile - ...
+ * @filename: ...
+ * @letters: ...
+ * Return: ...
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *ptr;
+	ssize_t des, a, b;
+
+	des = open(filename, O_RDONLY);
+	if (des == -1)
+		return (0);
+
+	ptr = malloc((sizeof(char)) * letters);
+	a = read(des, ptr, letters);
+	b = write(TSDOUT_FILENO, ptr, a);
+
+	free(ptr);
+	close(des);
+	return (b);
+}
