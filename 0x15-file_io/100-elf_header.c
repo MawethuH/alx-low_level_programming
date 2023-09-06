29dceb308c57abd66820c980e0674d455bf4c55d
MawethuH <hlophemawethu@gmail.com>
2023-09-06 16:04:08 -0700

    elf header

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - ...
+ * @argc: ...
+ * @argv: ...
+ * Return: ...
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc: %d, argv: %p\n", argc, (void *)*argv);
+	return (1);
+}
