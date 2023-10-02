21959a8e0406d4cdd901f3d9434c5b6eaa4e678f
MawethuH <hlophemawethu@gmail.com>
2023-10-02 14:45:50 -0700

    File handling

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,19 +8,18 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	char *buffer;
+	ssize_t fd, r, w;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
 
-	free(buf);
+	buffer = malloc(sizeof(char) * letters);
+	r = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	free(buffer);
 	close(fd);
 	return (w);
 }
